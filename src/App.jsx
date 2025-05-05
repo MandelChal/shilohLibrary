55707a9f2477421b00e84653cdfbdeb0bc3bb5ed
Hadas <hadasg2003@gmail.com>
2025-05-05 15:44:57 +0300

    fix App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,7 @@ import Login from "./components/Login"; // גם login מתיקיית components
 
 function App() {
   return (
+<<<<<<< HEAD
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -39,6 +40,22 @@ function App() {
       </Routes>
     </Router>
   );
+=======
+    <>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      <Login />
+      <p>
+        Edit <code>src/App.jsx</code> and save to test HMR
+      </p>
+
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+>>>>>>> 1242d5c (fix App.jsx)
 }
 
 export default App;
